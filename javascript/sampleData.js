4bcd4b67aa7256a1b987404f487462cb4298b707
mythili-e <mythili.eniyarasu@gmail.com>
2024-03-09 17:47:26 +0530

    initial commit

---
+++ javascript/sampleData.js
@@ -0,0 +1,8 @@
+export const name = "Jesse";
+export const age = 40;
+
+export const emp ={"employees":[
+    {"firstName":"John", "lastName":"Doe"},
+    {"firstName":"Anna", "lastName":"Smith"},
+    {"firstName":"Peter", "lastName":"Jones"}
+  ]}
